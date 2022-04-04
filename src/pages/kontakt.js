2d37fc95b2e1d40a9fcb75d196cd9fb7ad53de11
Tomek <tomasz.mizak99@gmail.com>
2022-04-04 19:36:42 +0200

    Zamieszczenie formularza wyceny

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
       <h2>Kontakt</h2>
       <p><b>Tomasz Mizak</b><br/>tel. 512-294-324<br/>email. serwis@strykowpc.pl</p>
       <h3>Poproś o wycenę korzystając z poniższego formularza</h3>
-
+      <iframe title="Formularz wyceny" src="https://docs.google.com/forms/d/e/1FAIpQLSe8vnDMvDZVQYokPzj83dbxcoo9IKAnleLGbz81rZBF1ygRCg/viewform?embedded=true" width="640" height="998" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
     </Layout>
   )
 }
