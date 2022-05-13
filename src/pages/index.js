9a794b0689ef3c3b68e16b3123a9b2cbdbefcea7
Tomek <tomasz.mizak99@gmail.com>
2022-05-13 21:13:04 +0200

    seo new 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,13 @@ const IndexPage = () => {
       <h2>Serwis komputerowy Stryków</h2>
       <p>Szukasz serwisu komputerowego w Strykowie? Zajmujemy się <b>profesjonalną naprawą i konserwacją sprzętu komputerowego</b>. <b>Zajmiemy się Twoim laptopem w Strykowie oraz okolicach</b>. Dojedziemy na miejsce pod wskazany adres i zrealizujemy naprawę na miejscu.</p>
       <h2>Naprawa laptopów i ich konserwacja</h2>
-      <p>Najwięcej akcji serwisowych związanych jest z naprawą laptopów. Nie ważne czy Twój laptop służy do pisania, rozrywki czy pracy - zajmiemy się nim najlepiej jak potrafimy. Każda usługa wykonywana jest w najstaraniejszy sposób. Zawsze trafne, <b>darmowe diagnozy</b> oraz <b>konkretne rozwiązania problemów</b>.</p>
+      <p>Najwięcej akcji serwisowych związanych jest z naprawą laptopów. Nie ważne czy Twój laptop służy do pisania, rozrywki czy pracy - zajmiemy się nim najlepiej jak potrafimy. Każda usługa wykonywana jest w najbardziej staranny sposób. Zawsze trafne, <b>darmowe diagnozy</b> oraz <b>konkretne rozwiązania problemów</b>.</p>
       <p>Najczęściej wykonujemy czyszczenie układów chłodzenia, wymiany klawiatury czy podzespołów. Bardzo często działamy na samym systemie operacyjnym, realizując instalacje nowego systemu operacyjnego czy migrowanie systemu z dysku na dysk.</p>
       <h2>Montaż, ulepszanie oraz naprawa komputerów stacjonarnych</h2>
       <p>Niekiedy istnieje potrzeba stworzenia wydajnej stacji roboczej, w realnej cenie oraz małych gabarytach, każde zlecenie złożenia komputera tworzone jest indywidualnie, kierując się na obecnych trendach podzespołów na rynku oraz preferencjach użytkownika.</p>
       <p>Jeżeli Twój sprzęt się nie uruchamia, straciłeś dane lub chcesz coś poprawić, zgłoś się do nas. W przypadku komputerów stacjonarnych możliwości są zazwyczaj o wiele bardziej rozbudowane niż w przypadku laptopów, co pozwala często rozwinąć skrzydła nawet wieloletnim komputerom z niskiej półki cenowej.</p>
       <h3>Rozwiązujemy problemy zdalnie</h3>
-      <p>Niektóre sprawy tyczą się głównie problemów z sytemem operacyjnym, przy obecnych aplikacjach oraz stałym połączeniu sieciowym możemy połączyć się zdalnie do klienta i rozwiązać zadany problem.</p>
+      <p>Niektóre sprawy tyczą się głównie problemów z systemem operacyjnym, przy obecnych aplikacjach oraz stałym połączeniu sieciowym możemy połączyć się zdalnie do klienta i rozwiązać zadany problem.</p>
       <h3>Pracujemy całodobowo</h3>
       <p>Głównym zakresem pracy jest 10:00 - 21:00, wykonywanie usług w innym niż przedstawiony wymiar pracy wiąże się z większymi kosztami, jednakże prowadzimy serwis w pełnym wymiarze godzin.</p>
       <h4>Odbiór urządzenia od klienta, realizacja na miejscu</h4>
